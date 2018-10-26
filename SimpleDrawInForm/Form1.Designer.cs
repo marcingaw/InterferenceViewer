bb60e8020973ea34a11642d9814a7ff573399908
Marcin Gawlik <marcin.gawlik@windowslive.com>
2018-10-26 21:49:35 +0200

    Porting the initial version, continued... Not a very intuitive versioning system :-(

--- SimpleDrawInForm/Form1.Designer.cs
+++ SimpleDrawInForm/Form1.Designer.cs
@@ -28,24 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonR = new System.Windows.Forms.Button();
+            this.buttonG = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.waveFadeRate = new System.Windows.Forms.TrackBar();
+            this.buttonReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.waveFadeRate)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonR
+            // 
+            this.buttonR.Location = new System.Drawing.Point(0, 0);
+            this.buttonR.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonR.Name = "buttonR";
+            this.buttonR.Size = new System.Drawing.Size(23, 19);
+            this.buttonR.TabIndex = 0;
+            this.buttonR.Text = "R";
+            this.buttonR.UseVisualStyleBackColor = true;
+            this.buttonR.Click += new System.EventHandler(this.buttonR_Click);
+            // 
+            // buttonG
+            // 
+            this.buttonG.Location = new System.Drawing.Point(0, 24);
+            this.buttonG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonG.Name = "buttonG";
+            this.buttonG.Size = new System.Drawing.Size(23, 19);
+            this.buttonG.TabIndex = 1;
+            this.buttonG.Text = "G";
+            this.buttonG.UseVisualStyleBackColor = true;
+            this.buttonG.Click += new System.EventHandler(this.buttonG_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(0, 47);
+            this.buttonB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(23, 19);
+            this.buttonB.TabIndex = 2;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = true;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // waveFadeRate
+            // 
+            this.waveFadeRate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.waveFadeRate.LargeChange = 3;
+            this.waveFadeRate.Location = new System.Drawing.Point(555, 0);
+            this.waveFadeRate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.waveFadeRate.Minimum = 1;
+            this.waveFadeRate.Name = "waveFadeRate";
+            this.waveFadeRate.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.waveFadeRate.Size = new System.Drawing.Size(45, 366);
+            this.waveFadeRate.TabIndex = 3;
+            this.waveFadeRate.Value = 1;
+            this.waveFadeRate.Scroll += new System.EventHandler(this.waveFadeRate_Scroll);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonReset.Location = new System.Drawing.Point(0, 343);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(51, 23);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.waveFadeRate);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonG);
+            this.Controls.Add(this.buttonR);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.waveFadeRate)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonR;
+        private System.Windows.Forms.Button buttonG;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.TrackBar waveFadeRate;
+        private System.Windows.Forms.Button buttonReset;
     }
 }
 
--- SimpleDrawInForm/Form1.cs
+++ SimpleDrawInForm/Form1.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SimpleDrawInForm
 {
     public partial class Form1 : Form
     {
-        private List<Tuple<float, float>> points = new List<Tuple<float, float>>();
+        private List<Tuple<float, float>>
+            pointsR = new List<Tuple<float, float>>(),
+            pointsG = new List<Tuple<float, float>>(),
+            pointsB = new List<Tuple<float, float>>(),
+            points = null;  // The current list of points to operate on.
+
+        private List<List<Tuple<float, float>>> allPoints = new List<List<Tuple<float, float>>>();
 
         private double distance(int x1, int y1, int x2, int y2)
         {
@@ -16,64 +23,206 @@ namespace SimpleDrawInForm
 
         public Form1()
         {
+            allPoints.Add(pointsR);
+            allPoints.Add(pointsG);
+            allPoints.Add(pointsB);
             InitializeComponent();
+        }
+
+        private struct BitmapFragment
+        {
+            public BitmapFragment(Bitmap bm, int dx, int dy, int width, int height, double fadeRate, Graphics gr)
+            {
+                this.bm = bm;
+                this.dx = dx;
+                this.dy = dy;
+                this.width = width;
+                this.height = height;
+                this.fadeRate = fadeRate;
+                this.gr = gr;
+            }
+
+            public readonly Bitmap bm;          // The bitmap to draw on.
+            public readonly int dx, dy;         // Coordinates of the upper left corner of 'bm' on the whole picture.
+            public readonly int width, height;  // Size of the whole picture.
+            public readonly double fadeRate;    // Wave fade rate for distance, 10% of 'waveFadeRate.Value'.
+
+            public readonly Graphics gr;        // The context where to eventually draw to image in.
+        }
+
+        private void drawFragment(BitmapFragment bf)
+        {
+            for (int x = bf.dx, xl = bf.dx + bf.bm.Width; x < xl; ++x)
+            {
+                for (int y = bf.dy, yl = bf.dy + bf.bm.Height; y < yl; ++y)
+                {
+                    int r = 0, g = 0, b = 0;
+
+                    foreach (List<Tuple<float, float>> pts in allPoints)
+                    {
+                        if (pts.Count > 0)
+                        {
+                            double wave = 0, waves = 0;
+
+                            foreach (Tuple<float, float> p in pts)
+                            {
+                                int px = (int)(p.Item1 * bf.width);
+                                int py = (int)(p.Item2 * bf.height);
+                                double dist = distance(x, y, px, py);
+                                wave += Math.Pow(bf.fadeRate, dist / 63) * Math.Cos(0.05 * dist);
+                                ++waves;
+                            }
+
+                            if (pts == pointsR)
+                            {
+                                r = (int)(127 + 127 * (wave / waves));
+                            }
+                            else if (pts == pointsG)
+                            {
+                                g = (int)(127 + 127 * (wave / waves));
+                            }
+                            else
+                            {
+                                b = (int)(127 + 127 * (wave / waves));
+                            }
+                        }
+                    }
+
+                    bf.bm.SetPixel(x - bf.dx, y - bf.dy, Color.FromArgb(r, g, b));
+                }
+            }
+
+            lock (bf.gr)
+            {
+                bf.gr.DrawImage(bf.bm, bf.dx, bf.dy);
+            }
+        }
+
+        private void drawFragmentObj(Object bitmapFragment)
+        {
+            drawFragment((BitmapFragment)bitmapFragment);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            RectangleF vcb = g.VisibleClipBounds;
+            const bool MULTITHREADED = true;
 
-            if (points.Count > 0)
+            Graphics gr = e.Graphics;
+            RectangleF vcb = gr.VisibleClipBounds;
+
+            if (MULTITHREADED)
             {
-                Bitmap bm = new Bitmap((int)vcb.Width, (int)vcb.Height);
+                const int DELTA = 100;
 
-                for (int x = 0, xl = bm.Width; x < xl; ++x)
+                int width = (int)vcb.Width;
+                int height = (int)vcb.Height;
+                List<Tuple<Thread, BitmapFragment>> threads = new List<Tuple<Thread, BitmapFragment>>();
+
+                for (int x = 0; x < width; x += DELTA)
                 {
-                    for (int y = 0, yl = bm.Height; y < yl; ++y)
+                    for (int y = 0; y < height; y += DELTA)
                     {
-                        double wave = 0, waves = 0;
-
-                        foreach (Tuple<float, float> p in points)
-                        {
-                            int px = (int)(p.Item1 * vcb.Width);
-                            int py = (int)(p.Item2 * vcb.Height);
-                            wave += Math.Sin(distance(x, y, px, py) / 10);
-                            ++waves;
-                        }
-
-                        int c = (int)(127 + 127 * (wave / waves));
-                        bm.SetPixel(x, y, Color.FromArgb(c, c, c));
+                        Thread thread = new Thread(drawFragmentObj);
+                        BitmapFragment bf = new BitmapFragment(
+                            new Bitmap(DELTA, DELTA), x, y, width, height, 0.1 * waveFadeRate.Value, gr);
+                        threads.Add(new Tuple<Thread,BitmapFragment>(thread, bf));
+                        thread.Start(bf);
                     }
                 }
 
-                g.DrawImage(bm, vcb);
+                foreach (Tuple<Thread, BitmapFragment> thread in threads) thread.Item1.Join();
+            }
+            else
+            {
+                drawFragment(new BitmapFragment(
+                    new Bitmap((int)vcb.Width, (int)vcb.Height),
+                    0, 0, (int)vcb.Width, (int)vcb.Height,
+                    0.1 * waveFadeRate.Value, gr));
             }
 
-            foreach (Tuple<float, float> p in points)
+            foreach (List<Tuple<float, float>> pts in allPoints)
             {
-                g.DrawEllipse(Pens.Red, new Rectangle
+                Pen pen = pts == pointsR ? Pens.DarkRed : pts == pointsG ? Pens.DarkGreen : Pens.DarkBlue;
+
+                foreach (Tuple<float, float> p in pts)
                 {
-                    X = (int)(p.Item1 * vcb.Width) - 5,
-                    Y = (int)(p.Item2 * vcb.Height) - 5,
-                    Width = 10,
-                    Height = 10
-                });
+                    gr.DrawEllipse(pen, new Rectangle
+                    {
+                        X = (int)(p.Item1 * vcb.Width) - 5,
+                        Y = (int)(p.Item2 * vcb.Height) - 5,
+                        Width = 10,
+                        Height = 10
+                    });
+                }
             }
+        }
+
+        private void makeBold(Button b)
+        {
+            b.Font = new Font(b.Font, b.Font.Style | FontStyle.Bold);
+        }
+
+        private void makeRegular(Button b)
+        {
+            b.Font = new Font(b.Font, b.Font.Style & ~FontStyle.Bold);
+        }
+
+        private void buttonR_Click(object sender, EventArgs e)
+        {
+            makeBold(buttonR);
+            makeRegular(buttonG);
+            makeRegular(buttonB);
+            points = pointsR;
+        }
+
+        private void buttonG_Click(object sender, EventArgs e)
+        {
+            makeRegular(buttonR);
+            makeBold(buttonG);
+            makeRegular(buttonB);
+            points = pointsG;
+        }
+
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            makeRegular(buttonR);
+            makeRegular(buttonG);
+            makeBold(buttonB);
+            points = pointsB;
+        }
+
+        private void waveFadeRate_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            makeRegular(buttonR);
+            makeRegular(buttonG);
+            makeRegular(buttonB);
+            points = null;
+            pointsR.Clear();
+            pointsG.Clear();
+            pointsB.Clear();
+            waveFadeRate.Value = 9;
+            Invalidate();
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            this.Invalidate();
+            Invalidate();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            waveFadeRate.Value = 9;
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+            if (points == null) return;
+
             Rectangle cr = this.ClientRectangle;
 
             foreach (Tuple<float, float> p in points)
@@ -84,7 +233,7 @@ namespace SimpleDrawInForm
                 if (distance(e.X, e.Y, x, y) < 5)
                 {
                     points.Remove(p);
-                    this.Invalidate();
+                    Invalidate();
                     return;
                 }
             }
@@ -94,7 +243,7 @@ namespace SimpleDrawInForm
                 (float)e.Y / cr.Height
             ));
 
-            this.Invalidate();
+            Invalidate();
         }
     }
 }
